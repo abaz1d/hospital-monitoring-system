aaed2b04d16a19f4347023f7a33b4f7d98abb889
Dimas <muhammaddimassuraji@gmail.com>
2025-09-14 16:08:48 +0700

    Update useDashboard.ts

--- src/composables/useDashboard.ts
+++ src/composables/useDashboard.ts
@@ -638,7 +638,7 @@ export const useDashboard = () => {
             borderWidth: 0,
             useHTML: true,
             format:
-              '<div style="text-align:center"><span style="font-size:25px;color:#374151">{y}</span><br/><span style="font-size:12px;opacity:0.4">Orang</span></div>'
+              '<div style="text-align:center"><span style="font-size:25px;color:#374151">{y}</span><br/><span style="font-size:12px;opacity:0.4">Pasien</span></div>'
           }
         }
       },
